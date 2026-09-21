4b26db000208f729baf670c585a8437baabe3e1d
brawlmytop228-eng <brawlmytop228@gmail.com>
2026-09-21 23:31:23 +0300

    server.js

--- server.js
+++ server.js
@@ -5,8 +5,11 @@ const { Server } = require("socket.io");
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
+const path = require("path");
 
-app.use(express.static("public"));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
 
 const users = new Map();
 
